1c5350660da4b5a0e1c42bf268d63ce3d3e14e2d
Mauro Lepore <maurolepore@gmail.com>
2023-10-18 21:26:04 +0000

    Sleep less

--- demo-task.r
+++ demo-task.r
@@ -6,6 +6,6 @@ for (i in seq_along(cols)) {
   path <- paste0(cols[i], ".csv")
   message("Writing ", path, " in ", getwd())
   readr::write_csv(data[i], path)
-  
-  Sys.sleep(5)
+
+  Sys.sleep(3)
 }
